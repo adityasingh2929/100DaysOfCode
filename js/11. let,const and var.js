b5bf7a0379ff88b9ba55f1e3f453083cda049a79
adityasingh2929 <2022aditya29@gmail.com>
2025-07-03 13:53:03 -0500

    Practiced questions for JS

--- js/11. let,const and var.js
+++ js/11. let,const and var.js
@@ -6,7 +6,6 @@ d = 5;
 // a = 1;
 // console.log(a); //Not Allowed.
 
-
 b = 100;
 console.log(b);
 
@@ -16,23 +15,20 @@ console.log(c);
 d = 500;
 console.log(d);
 
-
 // Now, there are few diff. ways to declare a var in JS, but it didnt know much about Scope.
 
-
-
-console.log("Hello World")
-console.log("The code has been executed up untill this point.")
+console.log("Hello World");
+console.log("The code has been executed up untill this point.");
 
 // VARIABLES, DATA TYPES AND OBJECT
 
 var aa = 5;
 var bb = 5;
-console.log(aa+bb)
+console.log(aa + bb);
 var cc = "Aditya";
-console.log(typeof(aa), typeof(bb), typeof(cc))
-console.log()
-console.log()
+console.log(typeof aa, typeof bb, typeof cc);
+console.log();
+console.log();
 
 // Variables can only contain either letters, digits, '$' or '_'.
 // variables can only start with letters,'$' and '_' (cant start with numbers).
@@ -47,38 +43,36 @@ console.log()
 // 1. 'const' is used when we've to keep certain values constant
 
 const avar = 6;
-console.log(avar)
+console.log(avar);
 // avar = avar + 1; this is not possible now.
-
 
 // VAR vs LET
 
-console.log()
-console.log()
+console.log();
+console.log();
 
-console.log("VAR vs LET:")
-console.log()
+console.log("VAR vs LET:");
+console.log();
 
-console.log("1. Var")
+console.log("1. Var");
 var x = 45;
-if(true){
-    var x = 33;
-    console.log(x)
+if (true) {
+  var x = 33;
+  console.log(x);
 }
-console.log(x)
+console.log(x);
 
-console.log()
-console.log()
+console.log();
+console.log();
 
-console.log("2. Let")
+console.log("2. Let");
 
 // decalaring y here with let means that its still locally scoped but in this case, its local -> global
 let y = 45;
-if(true){
-    let y = 33;
-    console.log(y)
+if (true) {
+  let y = 33;
+  console.log(y);
 }
-console.log(y)
+console.log(y);
 
-
-// TO PRINT MULTIPLE VALUES, WE USE 'console.table([val1,val2]);'  
+// TO PRINT MULTIPLE VALUES, WE USE 'console.table([val1,val2]);'
